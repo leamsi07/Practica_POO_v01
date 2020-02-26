3943521733911ee0438de7fdf720ec4a46e6b122
leamsi07 <61169746+leamsi07@users.noreply.github.com>
2020-02-26 11:18:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_POO_v01
+{
+    class MostrarVehiculo
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo MyCar = new Vehiculo();
+            MyCar.SetVehiculo("Jeep", "Grand Cherokee");
+            MyCar.VerVehiculo();
+            Console.WriteLine();
+            Vehiculo YourCar = new Vehiculo();
+            YourCar.SetVehiculo("Jeep", "Grand Cherokee SRT");
+            YourCar.VerVehiculo();
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_POO_v01
+{
+ class Vehiculo
+    {
+        string Marca = "Toyota";
+        string Modelo="Corrolla";
+        public void SetVehiculo( string marca, string modelo)
+        {   this.Marca=marca;
+            this.Modelo = modelo;
+        }
+        public void VerVehiculo()
+        {   Console.WriteLine("El vehículo es: " + Marca + " "+ Modelo);
+            Console.ReadLine();
+        }
+    }
+
+}
