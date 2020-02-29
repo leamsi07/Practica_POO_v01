55f16a3338439761e9ea31528f4e1f964c24592b
leamsi07 <61169746+leamsi07@users.noreply.github.com>
2020-02-29 01:35:38 -0400

    Add files via upload

---
+++ Aparment.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_POO_v02
+{
+    interface IAparmentBuilding
+    {
+        void Imprimir();
+        void Sell();
+        void Rent();
+    }
+    public class AparmentBuilding
+    {
+        //Atributos
+        protected string Address;
+        protected string Color;
+        protected string ConstruccionArea;
+        protected string Garage;
+        protected string Rooms;
+        protected string BathRooms;
+        public void Imprimir()
+        {
+            Console.WriteLine("Caracteristicas: ");
+            Console.WriteLine("Direccion " + Address);
+            Console.WriteLine("Color " + Color);
+            Console.WriteLine("Area de Construccion " + ConstruccionArea);
+            Console.WriteLine("Parqueo " + Garage);
+            Console.WriteLine("Habitaciones " + Rooms);
+            Console.WriteLine(" Baños " + BathRooms);
+            Console.ReadLine();
+        }
+        //Propiedades get/set
+        public string _Address { get => Address; set => Address = value; }
+        public string _Color { get => Color; set => Color = value; }
+        public string _ConstruccionArea { get => ConstruccionArea; set => ConstruccionArea = value; }
+        public string _Garage { get => Garage; set => Garage = value; }
+        public string _Rooms { get => Rooms; set => Rooms = value; }
+        public string _BathRooms { get => BathRooms; set => BathRooms = value; }
+    
+        //Metodos
+
+      
+        public void Sell()
+        {
+            Console.WriteLine("Apartamento Vendido!");
+            Console.WriteLine();
+            Console.WriteLine("Caracteristicas: ");
+            Console.WriteLine("Direccion " + Address);
+            Console.WriteLine("Color " + Color);
+            Console.WriteLine("Area de Construccion " + ConstruccionArea);
+            Console.WriteLine("Parqueo " + Garage);
+            Console.WriteLine("Habitaciones " + Rooms);
+            Console.WriteLine(" Baños " + BathRooms);
+            Console.ReadLine();
+        }
+        public void Rent()
+        {
+            Console.WriteLine("Apartamento Alquilado!");
+            Console.WriteLine();
+            Console.WriteLine("Caracteristicas: ");
+            Console.WriteLine("Direccion " + Address);
+            Console.WriteLine("Color " + Color);
+            Console.WriteLine("Area de Construccion " + ConstruccionArea);
+            Console.WriteLine("Parqueo " + Garage);
+            Console.WriteLine("Habitaciones " + Rooms);
+            Console.WriteLine(" Baños " + BathRooms);
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,21 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Practica_POO_v01
+namespace Practica_POO_v02
 {
-    class MostrarVehiculo
+    class Program
     {
         static void Main(string[] args)
         {
-            Vehiculo MyCar = new Vehiculo();
-            MyCar.SetVehiculo("Jeep", "Grand Cherokee");
-            MyCar.VerVehiculo();
-            Console.WriteLine();
-            Vehiculo YourCar = new Vehiculo();
-            YourCar.SetVehiculo("Jeep", "Grand Cherokee SRT");
-            YourCar.VerVehiculo();
-            Console.ReadKey();
+            AparmentBuilding aparment = new AparmentBuilding();
+            bool seguir = true;
+            string entrada = "";
+
+            while (seguir)
+            {
+                Console.Clear();
+                Console.WriteLine("Bienvenidos a el listado de empleados de la empresa ");
+                Console.WriteLine("Elija la opcion que desee: ");
+                Console.WriteLine("1 Datos apartamento ");
+                Console.WriteLine("2 Vender Apartamento ");
+                Console.WriteLine("3 Alquilar apartamento");
+                Console.WriteLine("4 Buscar Empleado");
+                entrada = Console.ReadLine();
+                switch (entrada)
+                {
+
+                    case "1":
+                        Console.Clear();
+
+                        Console.WriteLine("Agregar datos de Apartamento: ");
+                        Console.WriteLine("Direccion: ");
+                        aparment._Address = Console.ReadLine();
+                        Console.WriteLine("Color: ");
+                        aparment._Color = Console.ReadLine();
+                        Console.WriteLine("Area de construccion: ");
+                        aparment._ConstruccionArea = Console.ReadLine();
+                        Console.WriteLine("Parqueo: ");
+                        aparment._Garage = Console.ReadLine();
+                        Console.WriteLine("Habitaciones: ");
+                        aparment._Rooms = Console.ReadLine();
+                        Console.WriteLine("Baños: ");
+                        aparment._BathRooms = Console.ReadLine();
+                        aparment.Imprimir();
+                      
+                        break;
+
+                    case "2":
+
+                        Console.Clear();
+                        aparment.Sell();
+                        break;
+
+                    case "3":
+                        Console.Clear();
+
+                        aparment.Rent();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Adios :(");
+                        Console.ReadLine();
+                        seguir = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Algo anda mal :( elija la opcion correcta :)");
+                        Console.ReadLine();
+                        break;
+                     
+                }
+                Console.ReadKey();
+            }
         }
-       
     }
+
 }
